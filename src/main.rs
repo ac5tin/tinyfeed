c4249eedc6c0b5cdbb78cbcb9b5b775f8472deca
austin <austinchang4@gmail.com>
2022-10-05 09:07:10 +0800

    refactor: init db connection and launch app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+mod app;
+mod db;
+mod feed;
+mod utils;
+
+#[actix_web::main]
+async fn main() -> Result<(), anyhow::Error> {
+    // create databse connection
+    let conn = db::conn::get_conn().await?;
+
+    // start app
+    app::start(conn).await?;
+    Ok(())
 }
