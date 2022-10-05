daa74b5c12cd9cffa8606b195f29a0c40c76c03d
austin <austinchang4@gmail.com>
2022-10-05 08:59:58 +0800

    feat: get db connection

---
+++ src/db/conn.rs
@@ -0,0 +1,13 @@
+use crate::utils;
+use migration::{Migrator, MigratorTrait};
+use sea_orm::{ConnectOptions, DatabaseConnection};
+
+pub async fn get_conn() -> Result<DatabaseConnection, anyhow::Error> {
+    utils::fs::create_if_not_exist("./data.db")?;
+    // bad practise to use multiple connections when connecting to sqlite
+    let mut opts = ConnectOptions::new("sqlite://data.db".to_owned());
+    opts.sqlx_logging(false); // disable sqlx logging
+    let conn = sea_orm::Database::connect(opts).await?;
+    Migrator::up(&conn, None).await?;
+    Ok(conn)
+}
